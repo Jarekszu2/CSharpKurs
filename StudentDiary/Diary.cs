f8dec51537ae2e60edf973b71b2bf977f9ad6a2c
Jarekszu2 <jarekszu2@onet.pl>
2020-04-27 00:15:20 +0200

    Pierwszy obiekt.

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        // stan klasy (zmienne - pola)
+        List<float> ratings = new List<float>();
+
+        // zachowania klasy (metody)
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalculateAverage()
+        {
+            float sum = 0;
+            float avg = 0;
+            foreach (var item in ratings)
+            {
+                sum += item;
+            }
+
+            avg = sum / ratings.Count();
+
+            return avg;
+        }
+
+        public float GiveMaxRating()
+        {
+            return ratings.Max();
+        }
+
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+    }
+}
